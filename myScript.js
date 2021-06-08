65e16ee078c0fb3d14814462ef9156969dbde3e1
woratat <84006033+woratat@users.noreply.github.com>
2021-06-08 15:33:44 +0700

    02 Change message

--- myScript.js
+++ myScript.js
@@ -7,3 +7,22 @@ document.getElementById('ok').addEventListener('click',function(e){
     noElement.style.margin = ` 20px ${no_ma++}px`
     cancelElement.style.margin = ` 20px ${can_ma++}px`
 })
+
+document.getElementById('no').addEventListener('dblclick',function(e){ 
+    let okElement = document.getElementById('ok')
+    if(okElement.classList.contains('textNew1')){
+       okElement.innerHTML="วิชานี้"
+       okElement.classList.replace('textNew1','textNew2')
+    }else if(okElement.classList.contains('textNew2')){
+        okElement.innerHTML="ง่าย"
+        okElement.classList.replace('textNew2','textNew3')
+    }else if(okElement.classList.contains('textNew3')){
+        okElement.innerHTML="จริงๆนะ"
+        okElement.classList.replace('textNew3','textNew4')
+    }else if(okElement.classList.contains('textNew4')){
+        okElement.innerHTML="ok"
+        okElement.classList.replace('textNew4','textNew1')
+    }else{
+        okElement.classList.add('textNew1')
+    }
+})
